4ef0569ae0ec4946f7f7cb05d87cd143841ce765
Robby Tarigan <robby.tarigan@gmail.com>
2011-08-07 22:15:46 +0100

    Add Rubik class.

---
+++ src/Aqrubik/Rubik.cs
@@ -0,0 +1,15 @@
+﻿namespace Aqrubik.Web {
+    #region Using
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    #endregion Using
+    public class Rubik {
+        private int[] cubes = new int[27];
+
+        public int[] Cubes {
+            get { return cubes; }
+        }
+    }
+}
